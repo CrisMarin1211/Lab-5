6dc4d6411aa03c8096bb17b864799a4beedb431a
CrisMarin1211 <alca81411@gmail.com>
2024-04-12 18:46:18 -0500

    feat

--- dist/bundle.js
+++ dist/bundle.js
@@ -10,13 +10,23 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/components/Product/Product.ts":
+/*!*******************************************!*\
+  !*** ./src/components/Product/Product.ts ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Attribute: () => (/* binding */ Attribute),\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module './Product.css'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\nvar Attribute;\n(function (Attribute) {\n    Attribute[\"uid\"] = \"uid\";\n    Attribute[\"totle\"] = \"totle\";\n    Attribute[\"price\"] = \"price\";\n    Attribute[\"category\"] = \"category\";\n    Attribute[\"description\"] = \"description\";\n    Attribute[\"image\"] = \"image\";\n})(Attribute || (Attribute = {}));\nclass products extends HTMLElement {\n    constructor() {\n        super();\n        this.attachShadow({ mode: 'open' });\n    }\n    static get observedAttributes() {\n        const attrs = {\n            uid: null,\n            totle: null,\n            price: null,\n            category: null,\n            description: null,\n            image: null,\n        };\n        return Object.keys(attrs);\n    }\n    attributeChangedCallback(propName, oldValue, newValue = '') {\n        switch (propName) {\n            case Attribute.uid:\n                this.uid = newValue ? Number(newValue) : undefined;\n                break;\n            default:\n                this[propName] = newValue;\n                break;\n        }\n    }\n    connectedCallback() {\n        this.render();\n    }\n    render() {\n        var _a;\n        if (this.shadowRoot) {\n            this.shadowRoot.innerHTML = `\n\n                    <div class='info-container '>\n                        <p><b> Id: </b>${this.uid}</p>\n                        <p><b> Title: </b>${this.totle}</p>\n                        <p><b> Price: </b>${this.price}</p>\n                        <p><b> Category: </b>${this.category}</p>\n                        <p><b> Description: </b>${this.description}</p>\n                        <p><b> Image: </b>${this.image}</p>\n                    </div>\n               </section>\n               `;\n        }\n        const cssProfile = this.ownerDocument.createElement('style');\n        cssProfile.innerHTML = Object(function webpackMissingModule() { var e = new Error(\"Cannot find module './Product.css'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n        (_a = this.shadowRoot) === null || _a === void 0 ? void 0 : _a.appendChild(cssProfile);\n    }\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (products);\ncustomElements.define('product ', products);\n\n\n//# sourceURL=webpack://dca_scoffolding/./src/components/Product/Product.ts?");
+
+/***/ }),
+
 /***/ "./src/components/indexPadre.ts":
 /*!**************************************!*\
   !*** ./src/components/indexPadre.ts ***!
   \**************************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("\n\n\n//# sourceURL=webpack://dca_scoffolding/./src/components/indexPadre.ts?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Products: () => (/* reexport safe */ _Product_Product__WEBPACK_IMPORTED_MODULE_0__[\"default\"])\n/* harmony export */ });\n/* harmony import */ var _Product_Product__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Product/Product */ \"./src/components/Product/Product.ts\");\n\n\n\n//# sourceURL=webpack://dca_scoffolding/./src/components/indexPadre.ts?");
 
 /***/ }),
 
@@ -26,7 +36,7 @@ eval("\n\n\n//# sourceURL=webpack://dca_scoffolding/./src/components/indexPadre.
   \****************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_indexPadre__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/indexPadre */ \"./src/components/indexPadre.ts\");\n/* harmony import */ var _components_indexPadre__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_components_indexPadre__WEBPACK_IMPORTED_MODULE_0__);\n\nclass AppContainer extends HTMLElement {\n    constructor() {\n        super();\n        this.attachShadow({ mode: 'open' });\n    }\n    connectedCallback() {\n        this.render();\n    }\n    render() {\n        var _a;\n        const something = this.ownerDocument.createElement('div');\n        (_a = this.shadowRoot) === null || _a === void 0 ? void 0 : _a.appendChild(something);\n    }\n}\ncustomElements.define('app-container', AppContainer);\n\n\n//# sourceURL=webpack://dca_scoffolding/./src/indexAbuelo.ts?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_indexPadre__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/indexPadre */ \"./src/components/indexPadre.ts\");\n\nclass AppContainer extends HTMLElement {\n    constructor() {\n        super();\n        this.attachShadow({ mode: 'open' });\n    }\n    connectedCallback() {\n        this.render();\n    }\n    render() {\n        var _a;\n        const something = this.ownerDocument.createElement('div');\n        (_a = this.shadowRoot) === null || _a === void 0 ? void 0 : _a.appendChild(something);\n    }\n}\ncustomElements.define('app-container', AppContainer);\n\n\n//# sourceURL=webpack://dca_scoffolding/./src/indexAbuelo.ts?");
 
 /***/ })
 
@@ -57,18 +67,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _com
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
